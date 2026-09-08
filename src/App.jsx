5f4112d70bd45bfdf561dc1b7f5d1fafc9463b12
IT-Bug <Kevin-Kirchmeer@gmx.de>
2026-09-08 14:26:26 +0200

    Installed Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-
+import { Flex } from "@radix-ui/themes";
 
 export default function App() {
-  return <>
-  </>;
+  return (
+    <Flex direction="column">
+      <h1 className="text-red-500 text-2xl">Trattoria Bella</h1>
+    </Flex>
+  );
 }
